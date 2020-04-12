0df05f95b0594d9bb04dd1ea7a5bb1475d9635e0
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-04-11 19:43:50 -0500

    Final changes

--- bp-digit-classification.py
+++ bp-digit-classification.py
@@ -2,7 +2,7 @@ import struct as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(20)
+np.random.seed(0)
 
 def read_input(file):
     """
@@ -24,7 +24,7 @@ def read_input(file):
 train_size = 60000
 test_size = 10000
 input_size = 784
-hidden_neurons = 20
+hidden_neurons = 75
 output_neurons = 10
 
 train_X = read_input('train-images.idx3-ubyte')
@@ -33,9 +33,6 @@ test_X = read_input('t10k-images.idx3-ubyte')
 test_y = read_input('t10k-labels.idx1-ubyte')
 
 train_mean = np.mean(train_X[0:train_size].flatten())
-train_std = np.std(train_X[0:train_size].flatten())
-test_mean = np.mean(test_X[0:test_size].flatten())
-test_std = np.std(test_X[0:test_size].flatten())
 
 w_hidden = np.random.normal(loc=0, scale=1, size=(hidden_neurons, input_size))
 bias_hidden = np.random.normal(loc=1, scale=1, size=(hidden_neurons))
@@ -60,7 +57,7 @@ def der_sigmoid(induced_local_field):
     return sigmoid_u * (1 - sigmoid_u)
     
     
-eta = 0.1
+eta = 0.01
 epoch = 0
 train_energy_list = []
 train_error_list = []
@@ -71,7 +68,7 @@ while True:
     train_error = train_energy = 0
     for i in range(train_size):
         
-        x = (train_X[i].flatten() - train_mean) / train_std
+        x = (train_X[i].flatten() - train_mean) / 255
         
         # feedforward
         V = w_hidden @ x + bias_hidden
@@ -119,7 +116,7 @@ while True:
     test_error = test_energy = 0
     for j in range(test_size):
             
-        x = (test_X[j].flatten() - test_mean) / test_std
+        x = (test_X[j].flatten() - train_mean) / 255
         
         V = w_hidden @ x + bias_hidden
         y = tanh_act_fn(V)
@@ -147,23 +144,24 @@ while True:
     
     if train_energy_list[epoch] > train_energy_list[epoch-1]:
         eta *= 0.9
-    if test_error_list[epoch]/test_size < 0.05:
+    if train_error_list[epoch]/train_size < 0.01:
+        break   
+    if epoch == 85:
         break
-    epoch += 1   
-    if epoch == 1001:
-        break
+    epoch += 1
    
    
-plt.plot(range(len(train_error_list)), train_error_list, 'b.', label='Training Data')
-plt.plot(range(len(test_error_list)), test_error_list, 'r.', label='Testing Data')
+plt.plot(range(len(train_error_list)), train_error_list, 'b', label='Train Data')
+plt.plot(range(len(test_error_list)), test_error_list, 'r', label='Test Data')
+plt.axhline(y=500, c='black', linestyle=':')
 plt.title('Error vs Epoch')
 plt.xlabel('Number of Epochs')
 plt.ylabel('Number of Misclassifications')
 plt.legend()
 plt.show() 
 
-plt.plot(range(len(train_energy_list)), train_energy_list, 'b.', label='Training Data')
-plt.plot(range(len(test_energy_list)), test_energy_list, 'r.', label='Test Data')
+plt.plot(range(len(train_energy_list)), train_energy_list, 'b', label='Train Data')
+plt.plot(range(len(test_energy_list)), test_energy_list, 'r', label='Test Data')
 plt.title('Energy vs Epoch')
 plt.xlabel('Number of Epochs')
 plt.ylabel('Energy')
